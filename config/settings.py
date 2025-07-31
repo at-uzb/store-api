805387fcf9926ac493dbd16f4b79e2e3752ce538
Abdulaziz <tothebest2002@gmail.com>
2025-07-31 22:44:08 +0500

    Commit

--- config/settings.py
+++ config/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
     }
 }
 
