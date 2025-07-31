f27c071a032473985467d15e674452263a6aeddd
Abdulaziz <tothebest2002@gmail.com>
2025-07-31 22:32:18 +0500

    Commit

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True
